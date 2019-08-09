8b63f2df68a301f231ddd099ecf110ae92bace1c
Meawstarbuck <numoomo@hotmail.com>
2019-08-09 13:34:47 +0700

    Get-Post
    Get-Post API

--- demo/demo/src/main/java/com/example/demo/controller/SubjectController.java
+++ demo/demo/src/main/java/com/example/demo/controller/SubjectController.java
@@ -3,7 +3,9 @@ package com.example.demo.controller;
 import com.example.demo.model.Subject;
 import com.example.demo.service.SubjectService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -21,9 +23,4 @@ public class SubjectController {
         return subjectService.getSubjects();
     }
 
-    @PostMapping("/subjects")
-    public void addSubject(@RequestBody Subject subject){ }
-
 }
-
-
